78201934323bdc03694ec497a383653222b537a8
erwanledoux <lawanledoux@gmail.com>
2019-02-27 16:20:53 +0100

    npm ignored circle ci

--- jest-transformer.js
+++ jest-transformer.js
@@ -1,5 +1,10 @@
 const config = {
   babelrc: false,
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "transform-es2015-modules-commonjs"
+  ],
   presets: [
     [
       "@babel/env",
@@ -8,11 +13,6 @@ const config = {
       }
     ],
     "@babel/react"
-  ],
-  plugins: [
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    "transform-es2015-modules-commonjs"
   ]
 };
 module.exports = require("babel-jest").createTransformer(config);
