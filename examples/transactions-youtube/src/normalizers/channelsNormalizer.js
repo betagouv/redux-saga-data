137d2b5b4ecd747362e54d79c5982d4fdcf4a64d
erwanledoux <lawanledoux@gmail.com>
2019-08-19 16:12:35 +0200

    removed deprecated examples

--- examples/transactions-youtube/src/normalizers/channelsNormalizer.js
+++
@@ -1,5 +0,0 @@
-const channelsNormalizer = {
-  videos: 'videos'
-}
-
-export default channelsNormalizer
--- examples/transactions-youtube/src/selectors/selectSubscribedChannels.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect'
-
-const selectSubscribedChannels = createSelector(
-  state => state.data.channels,
-  channels => channels.filter(channel => channel.isSubscribed)
-)
-
-export default selectSubscribedChannels
