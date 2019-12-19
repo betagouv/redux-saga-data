800c648b041e1739820fbdfa30b4ce790bd1976e
Ahmed <aalaoui@octo.com>
2019-12-19 11:42:50 +0100

    add_markers_in_api_and_server_errors_payload

--- src/errors/handleApiError.js
+++ src/errors/handleApiError.js
@@ -3,7 +3,7 @@ import { call, put, select } from 'redux-saga/effects'
 
 export function* handleApiError(payload, config) {
   const { handleFail } = config
-
+  payload['error_type'] = 'API_ERROR'
   yield put(failData(payload, config))
 
   if (handleFail) {
